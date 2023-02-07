cd69927bd1872ae5532640b430e28431b6b5699a
wasim162010 <47940538+wasim162010@users.noreply.github.com>
2023-02-08 03:49:31 +0530

    Update Betting.js

--- BettingContract/test/Betting.js
+++ BettingContract/test/Betting.js
@@ -5,6 +5,7 @@ const { ethers, upgrades } = require("hardhat");
 describe("Test Betting contract scenarios", function () {
 
 
+
     it("Should deploy betting contract", async function() {
 
         const team1="India", team2="NZ";
@@ -21,6 +22,8 @@ describe("Test Betting contract scenarios", function () {
 
     })
 
+
+
     it("Should be able to place bet successfully", async function() {
 
         const team1="India", team2="NZ";
@@ -36,9 +39,11 @@ describe("Test Betting contract scenarios", function () {
         await bettingInst.connect(p2).placeBet(2,{ value: ethers.utils.parseEther("0.5") })
         const playerOneInfo = await bettingInst.playerInfo(addr1);
         const playerTwoInfo = await bettingInst.playerInfo(addr2);
+        // console.log(playerOneInfo)
+        // console.log(playerTwoInfo)
 
-        expect(ethers.utils.formatEther(playerOneInfo.betPlaced)).to.equal('0.5');
-        expect(ethers.utils.formatEther(playerTwoInfo.betPlaced)).to.equal('0.5');
+        expect(ethers.utils.formatEther(playerOneInfo.amountBet)).to.equal('0.5');
+        expect(ethers.utils.formatEther(playerTwoInfo.amountBet)).to.equal('0.5');
 
         const playerOneSelTeam = playerOneInfo.teamSelected.toString()
         const playerTwoSelTeam = playerTwoInfo.teamSelected.toString()
@@ -50,7 +55,9 @@ describe("Test Betting contract scenarios", function () {
 
     })
 
-    it("State transition should be fine", async function(){
+
+
+ it("State transition should be fine", async function(){
        
         const team1="India", team2="NZ";
         // let state="";
@@ -73,15 +80,17 @@ describe("Test Betting contract scenarios", function () {
         isEffectivetate =  await bettingInst.contractState();
         expect(isEffectivetate).to.equal("EFFECTIVE");
 
-        await bettingInst.connect(owner).endEventAndUpdateState(1)//(addr1)//(1) //player one should be winner in this case.
+        await bettingInst.connect(owner).endEventAndUpdateState(1)
         const endstate =  await bettingInst.contractState();
         expect(endstate).to.equal("MATURED");
 
-        await bettingInst.connect(owner).terminateContract()//(1) //player one should be winner in this case.
+        await bettingInst.connect(owner).terminateContract()
         const terminatestate =  await bettingInst.contractState();
         expect(terminatestate).to.equal("TERMINATED");
 
     })
+
+ 
 
     it("Complete flow: Both player agrees with winner", async function() {
 
@@ -90,34 +99,49 @@ describe("Test Betting contract scenarios", function () {
         const [owner, p1, p2] = await ethers.getSigners();
         const addr1 = await p1.address;
         const addr2 =  await p2.address;
+        // console.log(addr1)
+        // console.log(addr2)
   
   
         const betting = await ethers.getContractFactory("Betting");
         const bettingInst =  await upgrades.deployProxy(betting,[team1, team2], { initializer: 'initialize' });
+
     
         await bettingInst.connect(p1).placeBet(1,{ value: ethers.utils.parseEther("0.5") })
         const minBet = await bettingInst.minimumBet()
+       // console.log(" minBet ", minBet)
         await bettingInst.connect(p2).placeBet(2,{ value: ethers.utils.parseEther("0.5") })
     
+
         const playerOneBet = await bettingInst.playerInfo(addr1);
+        // console.log(" playerOneBet ", playerOneBet)
         const playerTwoBet = await bettingInst.playerInfo(addr2);
+        // console.log(" playerTwoBet ", playerTwoBet)
         
-        expect(ethers.utils.formatEther(playerOneBet.betPlaced)).to.equal('0.5');
-        expect(ethers.utils.formatEther(playerTwoBet.betPlaced)).to.equal('0.5');
-    
+        expect(ethers.utils.formatEther(playerOneBet.amountBet)).to.equal('0.5');
+        expect(ethers.utils.formatEther(playerTwoBet.amountBet)).to.equal('0.5');
+
         await bettingInst.connect(owner).startEventAndUpdateState()
         state =  await bettingInst.contractState();
+         //   console.log(" state ", state)
         expect(state).to.equal("EFFECTIVE");
         
-        await bettingInst.connect(owner).endEventAndUpdateState(1) //(addr1)//(1) //player one should be winner in this case.
+        await bettingInst.connect(owner).endEventAndUpdateState(1)
         const endstate =  await bettingInst.contractState();
+        //console.log(" endstate ", endstate)
         expect(endstate).to.equal("MATURED");
 
         //player one approves. Sca=enario when both player approves
         await bettingInst.connect(p1).approveWinner(true)
+        //console.log(" approved ",)
         await bettingInst.connect(p2).approveWinner(true) 
-        const playerOneApproval =  await bettingInst.playerOneApproval()
-        const playerTwoApproval =  await bettingInst.playerTwoApproval()
+        //console.log(" approved ",)
+
+        const playerOneApproval =  (await bettingInst.playerInfo(addr1)).playerApproval
+        //console.log(" playerOneApproval ",playerOneApproval)
+
+        const playerTwoApproval =  (await bettingInst.playerInfo(addr2)).playerApproval
+         //console.log(" playerTwoApproval ",playerTwoApproval)
         expect(playerOneApproval).to.equal(true)
         expect(playerTwoApproval).to.equal(true)
     
@@ -140,6 +164,8 @@ describe("Test Betting contract scenarios", function () {
         expect(postApprovalState).to.equal("SETTLED")
     
     })
+
+
 
     it("Complete flow: Players does not agree with the winner choice", async function() {
 
@@ -164,18 +190,19 @@ describe("Test Betting contract scenarios", function () {
         state =  await bettingInst.contractState();
         expect(state).to.equal("EFFECTIVE");
         
-        await bettingInst.connect(owner).endEventAndUpdateState(1) //(addr1)//(1) //No one is he winner.
+        await bettingInst.connect(owner).endEventAndUpdateState(1) 
         const endstate =  await bettingInst.contractState();
         expect(endstate).to.equal("MATURED");
 
-        const winner = await bettingInst.winningTeam();
-        expect(winner.toString() ==  1);
+        const winner = await bettingInst.matchObj().winningTeam 
+       // expect(winner.toString() ==  1);
+          expect(winner ==  1);
     
         //player one approves. Sca=enario when both player approves
         await bettingInst.connect(p1).approveWinner(false)
         await bettingInst.connect(p2).approveWinner(false) 
-        const playerOneApproval =  await bettingInst.playerOneApproval()
-        const playerTwoApproval =  await bettingInst.playerTwoApproval()
+        const playerOneApproval = (await bettingInst.playerInfo(addr1)).playerApproval
+        const playerTwoApproval = (await bettingInst.playerInfo(addr2)).playerApproval
         expect(playerOneApproval).to.equal(false)
         expect(playerTwoApproval).to.equal(false)
     
@@ -195,8 +222,9 @@ describe("Test Betting contract scenarios", function () {
         expect(postApprovalState).to.equal("TERMINATED")
     
       })
-      
-      it("Complete flow: Player bet for same team and approves the winner", async function() {
+
+    
+    it("Complete flow: Players does not agree with the winner choice", async function() {
 
         const team1="India", team2="NZ";
         let state="";
@@ -205,12 +233,13 @@ describe("Test Betting contract scenarios", function () {
         const addr2 =  await p2.address;
     
         const betting = await ethers.getContractFactory("Betting");
+        
         const bettingInst =  await upgrades.deployProxy(betting,[team1, team2], { initializer: 'initialize' });
     
         const preApprovalPlayerOneBal = await ethers.provider.getBalance(addr1)
         const preApprovalPlayerTwoBal = await ethers.provider.getBalance(addr2)
     
-        await bettingInst.connect(p1).placeBet(2,{ value: ethers.utils.parseEther("0.5") })
+        await bettingInst.connect(p1).placeBet(1,{ value: ethers.utils.parseEther("0.5") })
         const minBet = await bettingInst.minimumBet()
         await bettingInst.connect(p2).placeBet(2,{ value: ethers.utils.parseEther("0.5") })
     
@@ -218,36 +247,36 @@ describe("Test Betting contract scenarios", function () {
         state =  await bettingInst.contractState();
         expect(state).to.equal("EFFECTIVE");
         
-        await bettingInst.connect(owner).endEventAndUpdateState(2)//(addr2)//(2) 
+        await bettingInst.connect(owner).endEventAndUpdateState(1) 
         const endstate =  await bettingInst.contractState();
         expect(endstate).to.equal("MATURED");
 
-       const winner = await bettingInst.winningTeam();
-       expect(winner.toString() ==  2);
+        const winner = await bettingInst.matchObj().winningTeam 
+       // expect(winner.toString() ==  1);
+          expect(winner ==  1);
     
-        //player one approves. Scenario when both player approves
-        await bettingInst.connect(p1).approveWinner(true)
-        await bettingInst.connect(p2).approveWinner(true) 
-        const playerOneApproval =  await bettingInst.playerOneApproval()
-        const playerTwoApproval =  await bettingInst.playerTwoApproval()
-        expect(playerOneApproval).to.equal(true)
-        expect(playerTwoApproval).to.equal(true)
+        //player one approves. Sca=enario when both player approves
+        await bettingInst.connect(p1).approveWinner(false)
+        await bettingInst.connect(p2).approveWinner(false) 
+        const playerOneApproval =  (await bettingInst.playerInfo(addr1)).playerApproval
+        const playerTwoApproval =  (await bettingInst.playerInfo(addr2)).playerApproval
+        expect(playerOneApproval).to.equal(false)
+        expect(playerTwoApproval).to.equal(false)
     
         //check the approval, and transfer amount to the winner account
         const addr  =  await bettingInst.address
-
+        
         const preContBal = await ethers.provider.getBalance(addr)
-
+ 
         await bettingInst.connect(owner).checkApproval() 
         const postApprovalPlayerOneBal = await ethers.provider.getBalance(addr1)
         const postApprovalPlayerTwoBal = await ethers.provider.getBalance(addr2)
         const postContBal = await ethers.provider.getBalance(addr)
         const postApprovalState = await bettingInst.contractState()
-
+      
         expect(preApprovalPlayerOneBal ==  postApprovalPlayerOneBal);
         expect(preApprovalPlayerTwoBal ==  postApprovalPlayerTwoBal);
-        expect(postContBal.toString() == 0)
-        expect(postApprovalState).to.equal("SETTLED")
+        expect(postApprovalState).to.equal("TERMINATED")
     
       })
 
